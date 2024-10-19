b92a3ba3b7a255fcc93a7e7d30fe626ef2b32c95
Nam Nguyen <nam.nguyen.node@gmail.com>
2024-10-19 12:39:35 -0600

    Minimal yet complete ✨

--- src/signals.ts
+++ src/signals.ts
@@ -1,78 +1,41 @@
-type Subscription = {
-  run: () => void;
-  dependencies: Set<Set<Subscription>>;
-};
-
-const pendingSubscriptions: Set<Subscription> = new Set();
-
-const subscribe = (subscription: Subscription, subscriptionSet: Set<Subscription>) => {
-  subscriptionSet.add(subscription);
-  subscription.dependencies.add(subscriptionSet);
-};
+type Subscription = Function;
+let pendingSubscription: Subscription | null = null;
 
 export function signal<T>(initialValue: T) {
-  const subscriptionSet: Set<Subscription> = new Set();
-  let value = structuredClone(initialValue);
+  const subscriptions: Subscription[] = [];
+  let internalValue = structuredClone(initialValue);
 
   return {
     get value() {
-      if (pendingSubscriptions.size > 0) {
-        const lastSubscription = Array.from(pendingSubscriptions)[pendingSubscriptions.size - 1];
-        subscribe(lastSubscription, subscriptionSet);
+      if (pendingSubscription) {
+        subscriptions.push(pendingSubscription);
       }
-      return value;
+      return internalValue;
     },
-    set value(newValue) {
-      pendingSubscriptions.clear();
-      value = newValue;
-      for (const sub of subscriptionSet) {
-        sub.run();
+    set value(newValue: T) {
+      internalValue = structuredClone(newValue);
+      for (const subscription of subscriptions) {
+        subscription();
       }
     },
   };
 }
 
-const cleanup = (subscription: Subscription) => {
-  for (const dep of subscription.dependencies) {
-    dep.delete(subscription);
-  }
-  subscription.dependencies.clear();
-};
-
-export function effect(func: Function) {
-  const endEffect = startSubscription(func);
-  func();
+export function effect(fn: Function) {
+  const endEffect = startSubscription(fn);
+  fn();
   return endEffect();
 }
 
-export const startSubscription = (callback: Function) => {
-  const subscription: Subscription = {
-    run() {
-      callback();
-    },
-    dependencies: new Set(),
+export const startSubscription = (fn: Function) => {
+  pendingSubscription = fn;
+  return function endSubscription() {
+    pendingSubscription = null;
   };
-
-  pendingSubscriptions.add(subscription);
-
-  const endSubscription = () => {
-    pendingSubscriptions.delete(subscription);
-
-    return () => {
-      cleanup(subscription);
-    };
-  };
-
-  return endSubscription;
 };
 
 export function computed<T>(fn: () => T) {
   const signalResult = signal(undefined) as { value: T };
   effect(() => (signalResult.value = fn()));
-
-  return {
-    get value() {
-      return signalResult.value;
-    },
-  };
+  return signalResult;
 }
