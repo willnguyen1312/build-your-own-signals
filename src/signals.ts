2f1522846cee1db0a36ed5f792efd93bca0d9ff7
Nam Nguyen <nam.nguyen.node@gmail.com>
2024-10-19 12:42:04 -0600

    Bare minimum ✨

--- src/signals.ts
+++ src/signals.ts
@@ -2,6 +2,4 @@ export function signal<T>(initialValue: T) {}
 
 export function effect(fn: Function) {}
 
-export const startSubscription = (fn: Function) => {};
-
 export function computed<T>(fn: () => T) {}
