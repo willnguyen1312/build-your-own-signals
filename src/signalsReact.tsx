7ad80bc2b0109c91cc1cb36d328b26ca64cebe1c
Nam Nguyen <nam.nguyen.node@gmail.com>
2024-10-19 12:29:40 -0600

    ✨

--- src/signalsReact.tsx
+++ src/signalsReact.tsx
@@ -1,13 +1,1 @@
-import { useEffect, useReducer, useRef } from "react";
-import { startSubscription } from "./signals";
-
-export const useSignals = () => {
-  const rerender = useReducer((x) => x + 1, 0)[1];
-  const endSubscriptionRef = useRef<ReturnType<typeof startSubscription>>();
-
-  if (!endSubscriptionRef.current) {
-    endSubscriptionRef.current = startSubscription(rerender);
-  }
-
-  useEffect(() => endSubscriptionRef.current!(), []);
-};
+export const useSignals = () => {};
