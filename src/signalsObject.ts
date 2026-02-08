c3109616eb2cd414d50d044e41f4580631025798
Nam Nguyen <nam.nguyen.node@gmail.com>
2026-02-08 23:57:49 +0700

    🚀

--- src/signalsObject.ts
+++ src/signalsObject.ts
@@ -1,3 +1,5 @@
+import { useSignals } from "./signalsReactObject";
+
 type Subscription = {
   run: () => void;
   dependencies: Set<Set<Subscription>>;
@@ -10,26 +12,56 @@ const subscribe = (subscription: Subscription, subscriptionSet: Set<Subscription
   subscription.dependencies.add(subscriptionSet);
 };
 
-export function signal<T>(initialValue: T) {
-  const subscriptionSet: Set<Subscription> = new Set();
-  let value = structuredClone(initialValue);
+const SignalValue = function ({ data }: { data: any }) {
+  useSignals();
+  return data.value;
+};
 
-  return {
-    get value() {
-      if (pendingSubscriptions.size > 0) {
-        const lastSubscription = Array.from(pendingSubscriptions)[pendingSubscriptions.size - 1];
-        subscribe(lastSubscription, subscriptionSet);
-      }
-      return value;
-    },
-    set value(newValue) {
-      value = newValue;
-      for (const sub of subscriptionSet) {
-        sub.run();
-      }
-    },
-  };
+declare class Signal<T = unknown> {
+  get value(): T;
+  set value(value: T);
+  _value: T;
+  _subscriptionSet: Set<Subscription>;
+  constructor(initialValue: T);
 }
+
+function Signal<T = unknown>(this: Signal, initialValue: T) {
+  this._value = initialValue;
+  this._subscriptionSet = new Set<Subscription>();
+}
+
+export function signal<T = unknown>(initialValue?: T) {
+  return new Signal(initialValue);
+}
+
+Object.defineProperty(Signal.prototype, "value", {
+  get(this: Signal) {
+    if (pendingSubscriptions.size > 0) {
+      const lastSubscription = Array.from(pendingSubscriptions)[pendingSubscriptions.size - 1];
+      subscribe(lastSubscription, this._subscriptionSet);
+    }
+    return this._value;
+  },
+  set(this: Signal, newValue) {
+    this._value = newValue;
+    for (const sub of this._subscriptionSet) {
+      sub.run();
+    }
+  },
+});
+
+// Decorate Signals so React renders them as <SignalValue> components.
+Object.defineProperties(Signal.prototype, {
+  $$typeof: { configurable: true, value: Symbol.for("react.element") },
+  type: { configurable: true, value: SignalValue },
+  props: {
+    configurable: true,
+    get() {
+      return { data: this };
+    },
+  },
+  ref: { configurable: true, value: null },
+});
 
 const cleanup = (subscription: Subscription) => {
   for (const dep of subscription.dependencies) {
@@ -65,8 +97,8 @@ export const startSubscription = (callback: Function) => {
   return endSubscription;
 };
 
-export function computed<T>(fn: () => T) {
-  const signalResult = signal(undefined) as { value: T };
+export function computed<T = unknown>(fn: () => T): any {
+  const signalResult = signal();
   effect(() => (signalResult.value = fn()));
 
   return {
